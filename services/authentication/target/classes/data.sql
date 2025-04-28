0201983101257e0f95da384c14e533f497914484
Ashesh Saraf <asheeshs@banquemisr.com>
2025-04-28 12:46:12 +0530

    Cleaned target folder

--- services/authentication/target/classes/data.sql
+++
@@ -1,7 +0,0 @@
--- Insert roles only if they do not exist
-INSERT INTO roles (role_name)
-SELECT 'USER' WHERE NOT EXISTS (SELECT 1 FROM roles WHERE role_name = 'USER');
-
-INSERT INTO roles (role_name)
-SELECT 'ADMIN' WHERE NOT EXISTS (SELECT 1 FROM roles WHERE role_name = 'ADMIN');
-
--- services/authentication/target/classes/db.migration/V1.0.0__authentication_service_data.sql
+++
@@ -1,3 +0,0 @@
-insert into roles (name)
-values	('ROLE_USER'),
-		('ROLE_ADMIN');
